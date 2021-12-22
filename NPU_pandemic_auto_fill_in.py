615dd68878efe5bdfa32728f2666a2489cf46cf1
JellyBlack <jellyblack4553@gmail.com>
2021-12-23 01:37:08 +0800

    紧急修复：调整字段、签名校验
    1. 调整了一些填报的字段
    2. 访问时添加签名和时间戳，防止被检测
    3. 修复webhook报错问题

--- NPU_pandemic_auto_fill_in.py
+++ NPU_pandemic_auto_fill_in.py
@@ -1,12 +1,13 @@
 import sys
 import requests
 import json
+import re
 
 # 网页url
 yqtb_url = "http://yqtb.nwpu.edu.cn"
 uis_login_url = "https://uis.nwpu.edu.cn/cas/login?service=http%3A%2F%2Fyqtb.nwpu.edu.cn%2F%2Fsso%2Flogin.jsp%3FtargetUrl%3Dbase64aHR0cDovL3lxdGIubndwdS5lZHUuY24vL3d4L3hnL3l6LW1vYmlsZS9pbmRleC5qc3A%3D"
-yqtb_fillin_url = "http://yqtb.nwpu.edu.cn/wx/ry/ry_util.jsp"
-yqtb_referer_url = "http://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp"
+yqtb_fillin_url = "http://yqtb.nwpu.edu.cn/wx/ry/" # 获取到签名和时间戳后拼接上去
+yqtb_detail_url = "http://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp"
 
 # 疫情填报的会话ID
 yqtb_cookie = ""
@@ -23,7 +24,11 @@ error = ""
 name = sys.argv[1]
 studentId = sys.argv[2]
 password = sys.argv[3]
-webhook = sys.argv[4]
+webhook = ""
+try:
+    webhook = sys.argv[4]
+except:
+    webhook = ""
 
 # 打印学生信息
 print("西北工业大学 疫情自动填报Python脚本")
@@ -59,35 +64,20 @@ try:
 
     # 提交填报信息post的数据（神TM知道这些是啥）
     fillinData = {
-        "sfczbcqca" : "",
-        "czbcqcasjd" : "",
-        "sfczbcfhyy" : "",
-        "czbcfhyysjd" : "",
+        "hsjc" : 1,
+        "xasymt" : 1,
         "actionType" : "addRbxx",
         "userLoginId" : studentId,
-        "userName" : name,
         "szcsbm" : 1,
+        "bdzt" : 1,
         "szcsmc" : "在学校",
-        "sfjt" : 0,
-        "sfjtsm" : "",
-        "sfjcry" : 0,
-        "sfjcrysm" : "",
-        "sfjcqz" : 0,
         "sfyzz" : 0,
         "sfqz" : 0,
-        "ycqksm" : "",
-        "glqk" : 0,
-        "glksrq" : "",
-        "gljsrq" : "",
         "tbly" : "sso",
-        "glyy" : "",
         "qtqksm" : "",
-        "sfjcqzsm" : "",
-        "sfjkqk" : 0,
-        "jkqksm" : "",
-        "sfmtbg" : "",
+        "ycqksm" : "",
         "userType" : 2,
-        "qrlxzt" : ""
+        "userName" : name
     }
     # 填报所需要的header
     fillinHeader = {
@@ -98,20 +88,32 @@ try:
         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
         "Content-Type" : "application/x-www-form-urlencoded",
         "Origin" : yqtb_url,
-        "Referer" : yqtb_referer_url,
+        "Referer" : yqtb_detail_url,
         "Accept-Encoding" : "gzip, deflate",
         "Accept-Language" : "zh-CN,zh;q=0.9,en;q=0.8",
         "Cookie" : "JSESSIONID=" + yqtb_cookie
     }
+
+    # 获取提交所需要的签名和时间戳
+    response3 = session.get(yqtb_detail_url, headers = fillinHeader)
+    content = response3.content.decode("utf-8")
+    extract = re.findall("ry_util.jsp.*(?=')",content)[0] # 提取请求参数
+    yqtb_fillin_url += extract
+
     # 提交填报信息
-    response3 = session.post(yqtb_fillin_url, data = fillinData, headers = fillinHeader)
-    message = response3.text.strip().replace("\n", "").replace("\r", "").replace("－", "-") # 草（请赏析草字的妙处，6分）
+    response4 = session.post(yqtb_fillin_url, data = fillinData, headers = fillinHeader)
+    message = response4.text.strip().replace("\n", "").replace("\r", "").replace("－", "-") # 草（请赏析草字的妙处，6分）
     dict = json.loads(message)
     filled = True
     state = dict["state"]
     if state != "1":
         flag = False
-        error = dict["err-msg"]
+        if "err-msg" in dict:
+            error = dict["err-msg"]
+        elif "err_msg" in dict:
+            error = dict["err_msg"]
+        else:
+            error = "[疫情自动填报]无法访问错误信息"
 except:
     flag = False # 设置状态为失败
 
@@ -145,4 +147,7 @@ if len(webhook) != 0:
             "isAtAll" : not flag
         }
     }
-    response = requests.post(webhook, data = json.dumps(data), headers = {"Content-Type": "application/json"})
+    try:
+        requests.post(webhook, data = json.dumps(data), headers = {"Content-Type": "application/json"})
+    except:
+        print("发送钉钉通知失败")
